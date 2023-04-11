696e52d9d829d900d7891fce226a7cf73b0027e4
natalia <geracimova.ng@gmail.ru>
2023-04-11 16:06:46 +0300

    ф

---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,8 @@
+import PropTypes from 'prop-types';
+import css from './Feedback.module.css';
+
+class Feedback extends React.Component {
+  render() {
+    return <div className="Feedback"></div>;
+  }
+}
